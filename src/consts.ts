68c0b28594c3ec84d887ce67851461d37feda525
Gabor Heja <gheja@u2560p.git.stdio.hu>
2021-08-29 17:20:25 +0200

    Added types, consts, lib

---
+++ src/consts.ts
@@ -0,0 +1,2 @@
+// Gravitational constant, see: https://en.wikipedia.org/wiki/Gravitational_constant
+const gravConst = 6.67430e-11;
---
+++ src/lib.ts
@@ -0,0 +1,19 @@
+function dist2d(a: Vec2D, b: Vec2D)
+{
+	return Math.sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2);
+}
+
+function getAngle2D(a: Vec2D, b: Vec2D): number
+{
+	return Math.atan2(b.t - a.t, b.x - a.x);
+}
+
+function arrayPick(a: Array<any>)
+{
+	return a[Math.floor(Math.random() * a.length)];
+}
+
+function clamp(min: number, max: number, x: number)
+{
+	return Math.min(Math.max(x, min), max);
+}
