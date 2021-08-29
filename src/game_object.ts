60ddef97b44227ba0a15acfd29957f533c8e4ed2
Gabor Heja <gheja@u2560p.git.stdio.hu>
2021-08-30 01:15:35 +0200

    GameObject: storing diameter instead of radius

--- src/game_object.ts
+++ src/game_object.ts
@@ -2,13 +2,13 @@ class GameObject extends GravityBody
 {
 	icon: string;
 	color: string;
-	radius: number;
+	diameter: number;
 	
-	constructor(icon: string, name: string, color: string, position: Vec2D, velocity: Vec2D, mass: number, radius: number)
+	constructor(icon: string, name: string, color: string, position: Vec2D, velocity: Vec2D, mass: number, diameter: number)
 	{
 		super(name, position, velocity, mass);
 		this.icon = icon;
 		this.color = color;
-		this.radius = radius;
+		this.diameter = diameter;
 	}
 }
