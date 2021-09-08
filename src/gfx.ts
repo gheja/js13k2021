f149a82df84a9240aeb18a97e8521b3a4cc5a6a2
Gabor Heja <gheja@u2560p.git.stdio.hu>
2021-09-09 01:40:55 +0200

    Gfx: adjusted black hole graphics

--- src/gfx.ts
+++ src/gfx.ts
@@ -322,7 +322,7 @@ class Gfx
 		
 		size = PRERENDER_SIZE * 0.45;
 		
-		for (c of [ "#c07", "#f09", "#f44" "#f81" "#fc5" "#ffa", "#fff", "#000", "#111", "#000" ])
+		for (c of [ "#c07", "#e08", "#f44" "#f81" "#fc5", "#fff", "#000", "#111", "#000" ])
 		{
 			ctx.fillStyle = c;
 			ctx.beginPath();
