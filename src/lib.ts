5b4904ef4f33a1ce45e68b73f910d8e743faaaba
Gabor Heja <gheja@u2560p.git.stdio.hu>
2021-09-13 05:23:32 +0200

    Added st() to properly use 1st, 2nd, 3rd, nth

--- src/lib.ts
+++ src/lib.ts
@@ -47,3 +47,23 @@ function arrayShuffle(a) {
 		a[j] = x;
 	}
 }
+
+function st(n)
+{
+	if (n == 1)
+	{
+		return "1st";
+	}
+	else if (n == 2)
+	{
+		return "2nd";
+	}
+	else if (n == 3)
+	{
+		return "3rd";
+	}
+	else
+	{
+		return n + "th";
+	}
+}
