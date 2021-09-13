22a77a0b803b0f27807c923fea91bf7a69e71366
Gabor Heja <gheja@u2560p.git.stdio.hu>
2021-09-13 05:02:34 +0200

    Added externs.js

---
+++ src/externs.js
@@ -0,0 +1,7 @@
+let io = {};
+io.connect = function(a){};
+io.connect.on = function(a, b){};
+io.connect.emit = function(a, b){};
+io.listen = function(a){};
+io.sockets = {};
+io.sockets.socket = {};
